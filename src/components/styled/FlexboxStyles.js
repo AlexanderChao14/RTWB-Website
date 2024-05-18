a5270054a24b900ba16db38012f2b0a1c5205cfb
Joshua Ngo <joshua.ngo710@gmail.com>
2024-05-18 15:35:56 -0600

    Cleaned up styles, and added new section on Landing page

---
+++ src/components/styled/FlexboxStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FlexboxColumnCenter = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: steelblue;
+`;

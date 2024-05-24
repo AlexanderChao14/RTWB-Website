8ca5d78e7e1161b45315a1052425693f6863fdc9
Joshua Ngo <joshua.ngo710@gmail.com>
2024-05-24 15:13:37 -0600

    Added infocard component, fixed up CSS and styled the updates and more area on landing.

---
+++ src/components/InfoCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { MoreNewsCard } from './styled/ItemStyles'
+
+const descStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    backgroundColor: "lavender",
+    alignItems: "left",
+    width: "100%",
+    height: "100%",
+    padding: "15px"
+}
+
+const button = {
+    backgroundColor: "steelblue",
+    justifySelf: "left",
+    padding: "5px"
+}
+
+const InfoCard = ({title, desc}) => {
+  return (
+    <>
+        <MoreNewsCard className="grid-item-center items-center">
+            <p>Picture Here</p>
+            <div style={descStyle}>
+                <p style={{justifySelf:"left"}}>{title}</p>
+                <p style={{justifySelf:"left", margin:"10px 0px"}}>{desc}</p>
+                <div style={{margin:"10px 0px"}}>
+                    <button style={ button }>Read More</button>
+                </div>
+            </div>
+        </MoreNewsCard>
+    </>
+  )
+}
+
+export default InfoCard

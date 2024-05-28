2f7bf551eba74a6e28a6133160d646dc101d3b1c
Joshua Ngo <joshua.ngo710@gmail.com>
2024-05-28 14:47:37 -0600

    Added responsiveness and removed code redundancy

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import {CardHeading, SectionHeading} from '../styled/StyledHeading'
-import { GridStyle11, GridStyle12, GridStyle21 } from '../styled/GridStyles'
 import { TextMedium } from '../styled/TextStyles'
 import { FlexboxColumnCenter } from '../styled/FlexboxStyles'
-import { MoreNewsCard } from '../styled/ItemStyles'
 import InfoCard from '../InfoCard'
 
 
@@ -17,7 +15,7 @@ const Home = () => {
 
             {/* Introduction Section */}
             <section style={{backgroundColor: "orange"}}>   
-                <GridStyle11 className = "container-grid mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
+                <div className = "container-grid grid-cols-even mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
                     <div className="flex-item-dir-cols grid-item-center">
                         <CardHeading>We are RTWB</CardHeading>
                         <TextMedium>Mission: Improve respiratory health through educational advancement of local healthcare providers worldwide.</TextMedium>
@@ -28,12 +26,12 @@ const Home = () => {
                         <p>Picture goes here</p>
                     </div>
 
-                </GridStyle11>
+                </div>
             </section>
 
             {/* About us Second Section*/}
             <section style={{backgroundColor: "orange"}}>
-                <GridStyle12 className="container-grid mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
+                <div className="container-grid grid-cols-left mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
                     <div className="flex-item-dir-cols grid-item-center">
                         <CardHeading>About us</CardHeading>
                         <TextMedium>RTWB is a collaborative project to educate and empower health care practitioners initiated by Respiratory Therapists around the globe.</TextMedium>
@@ -43,12 +41,12 @@ const Home = () => {
                     <div className="flex-item-dir-cols grid-item-center items-center">
                         <p>Picture goes here</p>
                     </div>
-                </GridStyle12>
+                </div>
             </section>
             
             {/* Volunteer Section */}
             <section style={{backgroundColor: "orange"}}>
-                <GridStyle21 className="container-grid mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
+                <div className="container-grid grid-cols-right mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
                     <div className="flex-item-dir-cols grid-item-center items-center">
                         <CardHeading>Picture Goes Here</CardHeading>
                     </div>
@@ -58,13 +56,13 @@ const Home = () => {
                         <TextMedium>Interested in contributing to our cause? See how you can help our charity here:</TextMedium>
                         <div style={{backgroundColor: "steelblue", width:"30%"}}>Button: Get Involved</div>
                     </div>
-                </GridStyle21>
+                </div> 
             </section>
 
             {/* Make a dynamic grid here for more sections */}
             <section>
                 <FlexboxColumnCenter className="updatesContainer container mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-8">
-                    <SectionHeading>Updates and More</SectionHeading>
+                    <SectionHeading className="text-center">Updates and More</SectionHeading>
                     <div className="updatesGrid">
 
                         <InfoCard title="Meet the team" desc="Learn more about our incredible team."></InfoCard>

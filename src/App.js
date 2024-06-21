ef9d3559f263b78539b95d226a88f6d9041e62f8
Joshua Ngo <joshua.ngo710@gmail.com>
2024-06-20 18:39:32 -0600

    Added more pages, updated styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import {
 import Layout from './layout/Layout'
 import Home from './components/pages/Home'
 import AboutUs from './components/pages/AboutUs'
-import Volunteer from './components/pages/Volunteer'
+import GetInvolved from './components/pages/GetInvolved'
 import GetDeployed from './components/pages/GetDeployed'
+import HealthEducationPartner from './components/pages/HealthEducationPartner'
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout/>}>
               <Route path="/AboutUs" element={<AboutUs/>}/>
-              <Route path="/Volunteer" element={<Volunteer/>}/>
+              <Route path="/GetInvolved" element={<GetInvolved/>}/>
               <Route path="/GetDeployed" element={<GetDeployed/>}/>
+              <Route path="/HealthEducationPartner" element={<HealthEducationPartner/>}/>
               <Route index element={<Home/>}/>
             </Route>
           </Routes>
